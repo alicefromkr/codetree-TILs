44a999f9f15a7a7202d5b0191dfe17f6a0702476
Alice <30956835+alicefromkr@users.noreply.github.com>
2024-08-18 20:41:34 +0900

    Title: 장학금, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240818/장학금/scholarship.py
@@ -0,0 +1,12 @@
+numlist = input("")
+n = numlist.split(" ")
+mid = int(n[0])
+final = int(n[1])
+
+if mid >= 90:
+    if final >= 95:
+        print(100000)
+    if final >= 90:
+        print(50000)
+else:
+    print(0)
