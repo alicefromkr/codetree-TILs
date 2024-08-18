d08c83d65e34bb5458133f371e3a5564917d16fb
Alice <30956835+alicefromkr@users.noreply.github.com>
2024-08-18 20:42:06 +0900

    Title: 장학금, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240818/장학금/scholarship.py
+++ 240818/장학금/scholarship.py
@@ -6,7 +6,9 @@ final = int(n[1])
 if mid >= 90:
     if final >= 95:
         print(100000)
-    if final >= 90:
+    elif final >= 90:
         print(50000)
+    else:
+        print(0)
 else:
     print(0)
