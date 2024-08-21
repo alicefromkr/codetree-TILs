f3f48a1546817f7e8e223b63b1ee82b61042cf21
Alice <30956835+alicefromkr@users.noreply.github.com>
2024-08-21 13:24:27 +0900

    Title: 높은 수에서 낮은 수까지, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240821/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 240821/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -1,5 +1,9 @@
 num = input("").split(" ")
 a = int(num[0])
 b = int(num[1])
-for i in range(b, a-1, -1):
-    print(i, end=" ")
+if b >= a:
+    for i in range(b, a-1, -1):
+        print(i, end=" ")
+else:
+    for i in range(a, b-1, -1):
+            print(i, end=" ")
