1273ca2cd00721d9f4292e2bbc7979507f33b103
Alice <30956835+alicefromkr@users.noreply.github.com>
2024-09-01 04:05:20 +0900

    Title: 홀수만 출력하기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240901/홀수만 출력하기/print-only-odd-numbers.py
+++ 240901/홀수만 출력하기/print-only-odd-numbers.py
@@ -1,10 +1,5 @@
 n = int(input(""))
-arr = []
 for i in range(n):
     num = int(input(""))
     if num%3==0 and num%2==1:
-        arr.append(num)
-
-arr = sorted(arr)
-for x in arr:
-    print(x)
+        print(num)
